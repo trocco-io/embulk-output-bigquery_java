a7d4b501b4e4495cb58ab00e7f5155b1a9f691fa
giwa <ugw.gi.world@gmail.com>
2020-03-04 14:18:30 +0900

    remove unused import

--- src/main/java/org/embulk/output/bigquery_java/exception/BigqueryInternalException.java
+++ src/main/java/org/embulk/output/bigquery_java/exception/BigqueryInternalException.java
@@ -1,7 +1,5 @@
 package org.embulk.output.bigquery_java.exception;
 
-import com.google.api.services.bigquery.Bigquery;
-
 public class BigqueryInternalException extends BigqueryException {
     public BigqueryInternalException(String message){
         super(message);
