b0c980429f509229a9fb14b6f8bed18dede951e9
giwa <ugw.gi.world@gmail.com>
2020-05-03 18:06:26 +0900

    performance comparison

--- src/main/java/org/embulk/output/bigquery_java/BigqueryValueConverter.java
+++ src/main/java/org/embulk/output/bigquery_java/BigqueryValueConverter.java
@@ -9,16 +9,19 @@ import org.embulk.spi.time.TimestampParser;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BigqueryValueConverter {
     // default datetime format %Y-%m-%d %H:%M:%S.%6N
     // default timestamp format %Y-%m-%d %H:%M:%S.%6N
     // date format
     private static TimestampFormatter timestampFormat;
-
     private static String pattern;
     private static String timezone;
     private static TimestampParser parser;
     private static Timestamp ts;
+    private static Pattern datePattern = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");
 
     // TODO: refactor later
     public static void convertAndSet(ObjectNode node, String name, String src, BigqueryColumnOption columnOption, PluginTask task) {
@@ -52,6 +55,24 @@ public class BigqueryValueConverter {
                 node.put(name, timestampFormat.format(ts));
                 break;
             case DATE:
+                Matcher m = datePattern.matcher(src);
+                if (m.find() && true){
+                    node.put(name, src);
+                    break;
+                }
+
+                // if (true){
+                // Matcher m = datePattern.matcher(src);
+                // if (m.find()){
+                //     node.put(name, src);
+                //     break;
+                // }
+
+                // if (true){
+                //     node.put(name, src);
+                //     break;
+                // }
+
                 pattern = columnOption.getTimestampFormat().orElse(task.getDefaultTimestampFormat());
                 timezone = columnOption.getTimezone();
                 parser = TimestampParser.of(pattern, timezone);
