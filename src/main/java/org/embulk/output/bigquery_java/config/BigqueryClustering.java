4edc5bae32dfa601b7f4faed9fe295b8c7e10dd2
giwa <ugw.gi.world@gmail.com>
2021-11-15 08:01:21 +0900

    support clustering

--- src/main/java/org/embulk/output/bigquery_java/config/BigqueryClustering.java
+++ src/main/java/org/embulk/output/bigquery_java/config/BigqueryClustering.java
@@ -1,6 +1,7 @@
 package org.embulk.output.bigquery_java.config;
 
 import org.embulk.config.Config;
+import org.embulk.config.ConfigDefault;
 import org.embulk.config.Task;
 
 import java.util.List;
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 public interface BigqueryClustering extends Task {
     @Config("fields")
+    @ConfigDefault("null")
     public Optional<List<String>> getFields();
 }
